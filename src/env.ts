e20148776e6d2e05b4fbffe2fb32de5635c1b9dc
paulo <paulo_vicali@icloud.com>
2024-05-02 16:37:21 -0300

    feature(main): adiciona nova env

--- src/env.ts
+++ src/env.ts
@@ -2,6 +2,7 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   NEXT_PUBLIC_API_BASE_URL: z.string().url(),
+  NEXT_PUBLIC_COMPANY_NAME: z.string(),
 })
 
 const parsedEnv = envSchema.safeParse(process.env)
