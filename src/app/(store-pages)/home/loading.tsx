dabb485a34d992ae55bd6006c3f5980bc70073e2
paulo <paulo_vicali@icloud.com>
2024-05-02 16:39:33 -0300

    feature(main): cria modo de skeleton

--- src/app/(store-pages)/home/loading.tsx
+++ src/app/(store-pages)/home/loading.tsx
@@ -1,5 +1,13 @@
+import { Skeleton } from '@/app/components/skeleton'
+
 const LoadingHome = () => {
-  return <h1>Loading home...</h1>
+  return (
+    <div className="h-full grid grid-cols-9 grid-rows-6 gap-6">
+      <Skeleton className="col-span-6 row-span-6 rounded-lg h-[860px]" />
+      <Skeleton className="col-span-3 row-span-3 rounded-lg" />
+      <Skeleton className="col-span-3 row-span-3 rounded-lg" />
+    </div>
+  )
 }
 
 export default LoadingHome
--- src/app/utils/get-featured-products.ts
+++ src/app/utils/get-featured-products.ts
@@ -3,6 +3,8 @@ import { api } from '@/services/api'
 import { Products } from './types'
 
 export const getProductsFeatured = async (): Promise<Products[]> => {
+  await new Promise((resolve) => setTimeout(resolve, 2000))
+
   const response = await api('/featured')
   const products = await response.json()
 
