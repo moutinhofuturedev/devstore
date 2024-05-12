d087e42db6421580128176341eb60809c44933c0
paulo <paulo_vicali@icloud.com>
2024-05-11 23:31:35 -0300

    feature(main):gerando open graph image

---
+++ src/app/(store-pages)/products/[slug]/opengraph-image.tsx
@@ -0,0 +1,51 @@
+/* eslint-disable @next/next/no-img-element */
+import { ImageResponse } from 'next/og'
+import colors from 'tailwindcss/colors'
+
+import { getSlugProduct } from '@/app/utils/fetch-api/get-slug-product'
+import { env } from '@/env'
+
+export const alt = 'About Acme'
+export const size = {
+  width: 1200,
+  height: 630,
+}
+
+export const contentType = 'image/png'
+
+interface ProductSlug {
+  params: {
+    slug: string
+  }
+}
+
+const Image = async ({ params }: ProductSlug) => {
+  const { image, description } = await getSlugProduct(params.slug)
+  const productImageURL = new URL(image, env.NEXT_APP_IMAGE_URL).toString()
+
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <div
+        style={{
+          background: colors.zinc[950],
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <img
+          src={productImageURL}
+          alt={description}
+          style={{ width: '100%', height: '100%', objectFit: 'contain' }}
+        />
+      </div>
+    ),
+    {
+      ...size,
+    },
+  )
+}
+
+export default Image
--- src/env.ts
+++ src/env.ts
@@ -3,6 +3,7 @@ import { z } from 'zod'
 const envSchema = z.object({
   NEXT_PUBLIC_API_BASE_URL: z.string().url(),
   NEXT_PUBLIC_COMPANY_NAME: z.string(),
+  NEXT_APP_IMAGE_URL: z.string().url(),
 })
 
 const parsedEnv = envSchema.safeParse(process.env)
