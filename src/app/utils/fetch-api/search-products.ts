c63e49025a5e9d1cf51f30b77441da691930c040
paulo <paulo_vicali@icloud.com>
2024-05-15 00:32:11 -0300

    feature(main): fetch de dados api busca de produtos

---
+++ src/app/utils/fetch-api/search-products.ts
@@ -0,0 +1,16 @@
+import { api } from '@/services/api'
+
+import { Products } from '../types'
+
+export const searchProducts = async (query: string): Promise<Products[]> => {
+  await new Promise((resolve) => setTimeout(resolve, 2000))
+  const response = await api(`/products/search?q=${query}`, {
+    next: {
+      revalidate: 3600,
+    },
+  })
+
+  const products = await response.json()
+
+  return products
+}
