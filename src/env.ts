901c7bc62b9e8ddf11baabab5d34b7f4421fc437
paulo <paulo_vicali@icloud.com>
2024-05-20 07:38:35 -0300

    build [main]: config de Environments: .env no server e client

--- src/env.ts
+++ src/env.ts
@@ -1,20 +1,19 @@
+import { createEnv } from '@t3-oss/env-nextjs'
 import { z } from 'zod'
 
-const envSchema = z.object({
-  NEXT_PUBLIC_API_BASE_URL: z.string().url(),
-  NEXT_PUBLIC_COMPANY_NAME: z.string(),
-  NEXT_APP_IMAGE_URL: z.string().url(),
+export const env = createEnv({
+  server: {
+    NEXT_APP_IMAGE_URL: z.string().url(),
+  },
+
+  client: {
+    NEXT_PUBLIC_COMPANY_NAME: z.string(),
+    NEXT_PUBLIC_API_BASE_URL: z.string().url(),
+  },
+
+  runtimeEnv: {
+    NEXT_APP_IMAGE_URL: process.env.NEXT_APP_IMAGE_URL,
+    NEXT_PUBLIC_COMPANY_NAME: process.env.NEXT_PUBLIC_COMPANY_NAME,
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  },
 })
-
-const parsedEnv = envSchema.safeParse(process.env)
-
-if (!parsedEnv.success) {
-  console.error(
-    'Invalid environment variables:',
-    parsedEnv.error.flatten().fieldErrors,
-  )
-
-  throw new Error('Invalid environment variables')
-}
-
-export const env = parsedEnv.data
