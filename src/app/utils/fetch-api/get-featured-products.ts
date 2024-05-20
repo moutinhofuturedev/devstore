1dc3b5d22b1c037cb32099877fd2d04a3f7d3c0c
paulo <paulo_vicali@icloud.com>
2024-05-20 07:46:47 -0300

    refactor [main]: remove timeout

--- src/app/utils/fetch-api/get-featured-products.ts
+++ src/app/utils/fetch-api/get-featured-products.ts
@@ -3,7 +3,6 @@ import { api } from '@/services/api'
 import { Products } from '../types'
 
 export const getProductsFeatured = async (): Promise<Products[]> => {
-  await new Promise((resolve) => setTimeout(resolve, 2000))
   const response = await api('/products/featured', {
     next: {
       revalidate: 3600,
